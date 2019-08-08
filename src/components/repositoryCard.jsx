5aecc54f1e05f35b0a70f5f8f92dbe8a0e5a47f5
Nguyen, Zachary - CoSD/DSCo <zachary.nguyen@canada.ca>
2019-08-08 16:33:31 -0400

    - css changes

--- src/components/repositoryCard.jsx
+++ src/components/repositoryCard.jsx
@@ -7,6 +7,9 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Popover from '@material-ui/core/Popover';
+import PropTypes from "prop-types";
+
 
 const useStyles = makeStyles({
     card: {
@@ -17,11 +20,33 @@ const useStyles = makeStyles({
     },
     center: {
         textAlign: 'center',
+    },
+    btn: {
+        width: '100%',
+    },
+    typography: {
+        margin: 8,
     }
 });
 
-export default function RepositoryCard() {
+export default function RepositoryCard(props) {
     const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleClose() {
+        setAnchorEl(null);
+    }
+
+
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
 
     return (
         <Card className={classes.card}>
@@ -42,16 +67,36 @@ export default function RepositoryCard() {
                 </CardContent>
             </CardActionArea>
             <CardActions className={classes.center}>
-                <Button size="small" color="primary">
+                <Button href={('https://github.com/zachary-nguyen/' + props.repository + '/archive/master.zip')} download className={classes.btn} size="small" color="primary">
                     Download
                 </Button>
-                <Button size="small" color="primary">
+                <Button className={classes.btn} size="small" color="primary">
                     View Repository
                 </Button>
-                <Button size="small" color="primary">
+                <Button aria-describedby={id} onClick={handleClick} className={classes.btn} size="small" color="primary">
                     Clone
                 </Button>
+                <Popover
+                    id={id}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                >
+                    <Typography className={classes.typography}> <b>Use the following Url with Git or Svn to clone: </b> <br/> https://github.com/zachary-nguyen/{props.repository}.git</Typography>
+                </Popover>
             </CardActions>
         </Card>
     );
+}
+
+RepositoryCard.propTypes = {
+    repository: PropTypes.string
 }
--- src/views/homePage.jsx
+++ src/views/homePage.jsx
@@ -4,25 +4,41 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import axios from 'axios';
 
 function HomePage() {
+    const [repositories, setRepositories] = React.useState(null);
 
+    React.useEffect(() => {
+        axios.get('https://api.github.com/users/zachary-nguyen/repos')
+            .then(data=> {
+                console.log(data.data);
+                let repos = [];
+                // for(let i = 0; i< data.data.length; i++){
+                //     console.log(data.data[i])
+                // }
+                setRepositories(repos)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [repositories])
     return(
         <div className={'homepage'}>
             <div className={'homepage-container'}>
-                <Table padding={'none'}>
+                <Table className={'table-mrg'}>
                     <TableBody>
                         <TableRow>
-                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard/></TableCell>
-                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard/></TableCell>
+                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard repository={'InstantMail'}/></TableCell>
+                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard repository={'InstantMail'} /></TableCell>
+                        </TableRow>
+                        <TableRow className={'table-mrg'} >
+                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard repository={'InstantMail'}/></TableCell>
+                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard repository={'InstantMail'}/></TableCell>
                         </TableRow>
                         <TableRow>
-                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard/></TableCell>
-                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard/></TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard/></TableCell>
-                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard/></TableCell>
+                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard repository={'InstantMail'}/></TableCell>
+                            <TableCell size={'small'} align={'center'} padding={'none'}><RepositoryCard repository={'InstantMail'}/></TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
