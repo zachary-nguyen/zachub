47923a64270f4f0deafcf19cf4a1f38f48799cbc
Nguyen, Zachary - CoSD/DSCo <zachary.nguyen@canada.ca>
2019-09-19 16:05:11 -0400

    - underline animation

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -5,9 +5,9 @@ import Social from "../Social/social";
 const Navigation = () => {
     return(
         <div id={'nav-menu'}>
-            <Link to={'/all'}>Portfolio</Link>
-            <Link to={'/playground'}>Playground</Link>
-            <Link to={'/contact'}>Contact</Link>
+            <Link className={'nav-menu-a'} to={'/all'}>Portfolio</Link>
+            <Link className={'nav-menu-a'} to={'/playground'}>Playground</Link>
+            <Link className={'nav-menu-a'} to={'/contact'}>Contact</Link>
             <Social/>
         </div>
     );
--- src/components/Social/social.jsx
+++ src/components/Social/social.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faLinkedin, faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 const Social = () => {
@@ -9,6 +10,7 @@ const Social = () => {
             <a href="https://github.com/zachary-nguyen" target="_blank"><FontAwesomeIcon icon={faGithub}/></a>
             <a href="https://github.com/zachary-nguyen" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a>
             <a href="https://github.com/zachary-nguyen" target="_blank"><FontAwesomeIcon icon={faDiscord}/></a>
+            <a href="mailto:zack.nguyen@hotmail.com"><FontAwesomeIcon icon={faEnvelope}/></a>
         </div>
     )
 };
