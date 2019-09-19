02b4d6f9e891a71cedc910d772527af419133d5f
Nguyen, Zachary - CoSD/DSCo <zachary.nguyen@canada.ca>
2019-09-19 15:04:35 -0400

    - added about/skills

---
+++ src/views/Skills/skills.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Skills = () => {
+
+    return(
+        <div id="skills">
+            <h2 className="heading">Skills</h2>
+            <ul>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>Java</li>
+                <li>C</li>
+                <li>Node.js</li>
+                <li>ReactJs</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Python</li>
+                <li>Spring</li>
+                <li>Hibernate</li>
+                <li>Cypress</li>
+                <li>JUnit5</li>
+                <li>GraphQL</li>
+                <li>Struts2</li>
+                <li>ExpressJs</li>
+                <li>Electron</li>
+                <li>jQuery</li>
+                <li>SQL</li>
+                <li>SQLite</li>
+                <li>Oracle</li>
+                <li>Git</li>
+                <li>Maven</li>
+                <li>Jira</li>
+                <li>Jenkins</li>
+                <li>Webpack</li>
+                <li>SOAP</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Skills;
