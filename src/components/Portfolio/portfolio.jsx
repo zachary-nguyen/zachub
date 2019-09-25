7b16a6e277ee08e3b02ca9f4edb2e7104df9ffef
Zachary <zacnguyen98@gmail.com>
2019-09-24 21:26:03 -0400

    -css

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -56,7 +56,7 @@ const Portfolio = () => {
               </p>
               <Technology repo={repo.name}/>
               <br/>
-              <a href={'https://github.com/zachary-nguyen/' + repo.name} target={'_blank'} className="btn-rounded-white">View Source</a>
+              <a href={'https://github.com/zachary-nguyen/' + repo.name} target={'_blank'} className="view-src">View Source</a>
             </VerticalTimelineElement>
           ))}
         </VerticalTimeline> : null
